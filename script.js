6575694a19bb70a067b7cc1cead03ef8d34d1df0
selimzade <wselimzade@mail.ru>
2023-05-15 05:18:04 +0400

    +

---
+++ script.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const themeButton = document.querySelector(".theme-icon");
+const link = document.querySelector("link[href='style.css']");
+
+const changeTheme = () => {
+  if (link.getAttribute("href") == "style.css") {
+    link.setAttribute("href", "night-mode.css");
+  } else {
+    link.setAttribute("href", "style.css");
+  }
+};
+themeButton.addEventListener("click", changeTheme);
+
+const cv = document.querySelector(".cv");
+
+const downloadCV = () => {
+  const fileUrl = "./MyCV.pdf";
+  const fileName = "MyCv.pdf";
+  const link = document.createElement("a");
+  link.href = fileUrl;
+  link.download = fileName;
+  link.click();
+};
+cv.addEventListener("click", downloadCV);
+
+const contact = document.querySelector(".contact");
+
+const sendMail = () => {
+  const emailAddress = "wselimzade@mail.ru";
+  const emailSubject = "Portfolio Mail";
+  const emailBody = "Jobs";
+  window.location.href = `mailto:${emailAddress}?subject=${emailSubject}&body=${emailBody}`;
+};
+contact.addEventListener("click", sendMail);
