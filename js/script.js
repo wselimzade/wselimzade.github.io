e6dcf4fcc33e87788d73d2c2b1b30850eeeb3976
selimzade <wselimzade@mail.ru>
2023-05-16 09:57:31 +0400

    version+

---
+++ js/script.js
@@ -0,0 +1,60 @@
+"use strict";
+
+// change theme dark & night
+const themeButton = document.querySelector(".theme-icon");
+const link = document.querySelector("link[href='css/style.css']");
+const changeTheme = () => {
+  if (link.getAttribute("href") == "css/style.css") {
+    link.setAttribute("href", "css/night-mode.css");
+  } else {
+    link.setAttribute("href", "css/style.css");
+  }
+};
+themeButton.addEventListener("click", changeTheme);
+themeButton.addEventListener("touchstart", changeTheme);
+
+// download cv
+const cv = document.querySelector(".cv");
+const downloadCV = () => {
+  const fileUrl = "./cv/MyCV.pdf";
+  const fileName = "MyCV.pdf";
+  const link = document.createElement("a");
+  link.href = fileUrl;
+  link.download = fileName;
+  link.click();
+};
+cv.addEventListener("click", downloadCV);
+cv.addEventListener("touchstart", downloadCV);
+
+// contact to me
+const contact = document.querySelector(".contact");
+const sendMail = () => {
+  const emailAddress = "wselimzade@mail.ru";
+  const emailSubject = "Portfolio Mail";
+  const emailBody = "Replace this text with the desired email body content";
+  const encodedSubject = encodeURIComponent(emailSubject);
+  const encodedBody = encodeURIComponent(emailBody);
+  window.location.href = `mailto:${emailAddress}?subject=${encodedSubject}&body=${encodedBody}`;
+};
+contact.addEventListener("click", sendMail);
+contact.addEventListener("touchstart", sendMail);
+
+// change portfolio and skills section
+const skills = document.querySelector(".skills");
+const mySkills = document.querySelector(".my-skills");
+const portfolio = document.querySelector(".portfolio");
+const projects = document.querySelector(".projects");
+const openSkills = () => {
+  projects.style.display = "none";
+  mySkills.style.display = "block";
+  portfolio.classList.remove("active");
+  skills.classList.add("active");
+};
+const openProjects = () => {
+  projects.style.display = "block";
+  mySkills.style.display = "none";
+  portfolio.classList.add("active");
+  skills.classList.remove("active");
+};
+skills.addEventListener("click", openSkills);
+portfolio.addEventListener("click", openProjects);
